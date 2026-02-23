3ac2ead488c76929c6853365d2e19b9f45f70d74
SadKleTka <kadinovaman321123@gmail.com>
2026-02-23 09:12:58 +0600

    deleted some blanks

--- src/main/java/com/example/Spring/LMS/mapper/CourseMapper.java
+++ src/main/java/com/example/Spring/LMS/mapper/CourseMapper.java
@@ -4,8 +4,6 @@ import com.example.Spring.LMS.course.CourseEntity;
 import com.example.Spring.LMS.course.dto.CourseResponse;
 import com.example.Spring.LMS.course.dto.CourseToCreate;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
